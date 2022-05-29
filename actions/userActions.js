51b508d00718c9430d906c57f21c064b502124f9
Eric Whale <hsh048148@gmail.com>
2022-05-29 18:22:54 +0900

    Reset DB by connection issue

--- actions/userActions.js
+++ actions/userActions.js
@@ -73,7 +73,6 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/self
 // @access Private
 const getSelf = asyncHandler(async (req, res) => {
-  // TODO
   res.status(200).json(req.user);
 });
 
--- config/db.js
+++ config/db.js
@@ -6,6 +6,9 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
+    console.log(
+      `Error occured while connecting to the mongoDB`.magenta.underline
+    );
     console.log(error);
     process.exit(1);
   }
